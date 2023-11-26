084b5b7a27e26c12b521d87db9ce8cd514ea153d
ph0uad <saffarfouad@gmail.com>
2023-11-25 21:56:04 -0500

    added some stuff

--- index.js
+++ index.js
@@ -1,18 +1,67 @@
+// index.js
 
 // Function definitions for arming and disarming the system
 function armSystem() {
     console.log('System armed.');
-    // Actual code for arming the system (communication with Arduino)
+    updateSystemStatus('Armed');
+    // Add actual code to arm the system here
   }
   
   function disarmSystem() {
     console.log('System disarmed.');
-    // Actual code for disarming the system (communication with Arduino)
+    updateSystemStatus('Disarmed');
+    // Add actual code to disarm the system here
+  }
+  
+  // Update the system status on the homepage
+  function updateSystemStatus(status) {
+    const statusElement = document.getElementById('system-status');
+    if (statusElement) {
+      statusElement.textContent = status;
+    }
+    localStorage.setItem('systemStatus', status);
+  }
+  
+  // Function to check system status based on the activation times
+  function checkSystemStatus() {
+    const activationStart = localStorage.getItem('activationStart');
+    const activationEnd = localStorage.getItem('activationEnd');
+    
+    if (!activationStart || !activationEnd) {
+      // If times are not set, or settings have not been saved yet, default to disarmed
+      updateSystemStatus('Disarmed');
+      return;
+    }
+  
+    // Get the user's local timezone
+    const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  
+    // Create date objects using the local timezone
+    const now = new Date(new Date().toLocaleString('en-US', { timeZone: timezone }));
+    const start = new Date();
+    const [startHours, startMinutes] = activationStart.split(':');
+    start.setHours(startHours, startMinutes, 0, 0);
+  
+    const end = new Date();
+    const [endHours, endMinutes] = activationEnd.split(':');
+    end.setHours(endHours, endMinutes, 0, 0);
+  
+    // Arm or disarm the system based on the current time
+    if (now >= start && now < end) {
+      armSystem();
+    } else {
+      disarmSystem();
+    }
   }
   
   // Event listener for form submission on Homepage
   document.addEventListener('DOMContentLoaded', function() {
-    const immediateActionForm = document.querySelector('form[action="/submit-preferences"]');
+    // Update the system status text on load if element exists
+    const systemStatus = localStorage.getItem('systemStatus');
+    updateSystemStatus(systemStatus || 'Disarmed');
+  
+    // Listener for the immediate actions form
+    const immediateActionForm = document.getElementById('immediate-action-form');
     if (immediateActionForm) {
       immediateActionForm.addEventListener('submit', function(event) {
         event.preventDefault();
@@ -22,12 +71,13 @@ function armSystem() {
         } else if (action === 'disarm') {
           disarmSystem();
         }
-  
-        // After action, save the status to local storage (this needs to be changed to a server)
-        localStorage.setItem('systemStatus', action);
       });
     }
   
-    // 
+    // Set an interval to check the system status every minute
+    setInterval(checkSystemStatus, 60000);
+  
+    // Also check the system status immediately when the page loads
+    checkSystemStatus();
   });
   
